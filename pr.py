171eba7187355aaa33f37e1af02e920cc188b7e0
Dmitry <dimonuser72@gmail.com>
2024-10-23 23:26:49 +0300

    dataclass

--- pr.py
+++ pr.py
@@ -5,3 +5,33 @@ print('my_project')
 print('new line!!!')
 def some_function(number: Union[int, float]) -> None:
     pass
+
+import requests
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Coordinates:
+    latitude: float
+    longitude: float
+
+@dataclass
+class IssRequest:
+    message: str
+    iss_position: field(default_factory = dict)
+    timestamp: int
+
+    def __post_init__(self):
+        self.iss_position = Coordinates(*self.iss_position.values())
+
+
+
+api_url = 'http://api.open-notify.org/iss-now.json'
+
+response = requests.get(api_url)
+result = response.json()
+if response.status_code == 200:
+    iss = IssRequest(*result.values())
+    print(iss.iss_position.longitude)
+else:
+    print(response.status_code)
